8a4ce6af2ea7978d9693a0711a8a8640f3ba601f
gimhan <gimhankolonne@gmail.com>
2025-03-11 17:08:18 +0530

    Fix Google Sheets service authentication to handle newline characters in private key

--- app/Services/GoogleSheetsService.php
+++ app/Services/GoogleSheetsService.php
@@ -24,7 +24,7 @@ class GoogleSheetsService
             'type' => 'service_account',
             'project_id' => env('GOOGLE_PROJECT_ID'),
             'private_key_id' => env('GOOGLE_PRIVATE_KEY_ID'),
-            'private_key' => env('GOOGLE_PRIVATE_KEY'),
+            'private_key' => str_replace('\\n', "\n", env('GOOGLE_PRIVATE_KEY')),
             'client_email' => env('GOOGLE_CLIENT_EMAIL'),
             'client_id' => env('GOOGLE_CLIENT_ID'),
             'auth_uri' => env('GOOGLE_AUTH_URI'),
